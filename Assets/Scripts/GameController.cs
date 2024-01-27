4ad247bd9f096f3d8830a7f34a06b5b40ab45b2f
ssiannas <ssiannas@gmail.com>
2024-01-27 16:01:07 +0200

    Add currency system

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,6 +8,15 @@ namespace the_haha
         public delegate void GameOverDelegate();
         public event GameOverDelegate OnGameOver;
         private bool _isDecrementing = true;
+        
+        [SerializeField, InspectorName("Haha bucks")]
+        private int currency = 0;
+        [SerializeField]
+        private int currencyPerTick = 1;
+        [SerializeField]
+        private bool _isInDungeon = false;
+        
+        
         // Start is called before the first frame update
         private new void Awake()
         {
@@ -18,8 +27,18 @@ namespace the_haha
         private void Update()
         {
             if (_isDecrementing) InterestMeterController.Instance.DecrementInterestLevelTick();
+            currency += currencyPerTick * (int)Time.deltaTime;
         }
 
+        public void EnterDungeon()
+        {
+            // Load dungeon scene
+            // start timers
+            _isInDungeon = true;
+            _isDecrementing = true;
+           InterestMeterController.Instance.ShowInterestMeter(); 
+        }
+        
         public void GameOver()
         {
             _isDecrementing = false;
