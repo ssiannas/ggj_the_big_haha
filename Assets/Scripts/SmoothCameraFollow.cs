46d64085ea592ecdd1fd99782cb75b32474d8832
ssiannas <ssiannas@gmail.com>
2024-01-28 00:07:54 +0200

    Fix camera update

--- Assets/Scripts/SmoothCameraFollow.cs
+++ Assets/Scripts/SmoothCameraFollow.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace the_haha
@@ -7,12 +8,19 @@ namespace the_haha
     {
         public Transform player;  // Reference to the player's transform
         private Vector3 _velocity = Vector3.zero;
-        
+        private Vector3 _offset = new Vector3(0, 7, -3);
+
+        private void Awake()
+        {
+            transform.rotation = Quaternion.Euler(60, 0, 0);
+        }
+
         void LateUpdate()
         {
             if (!player) return;
-            Vector3 desiredPosition = player.position;
-            transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime);
+            var pos = player.position;
+            Vector3 desiredPosition = new Vector3(pos.x + _offset.x, _offset.y, pos.z + _offset.z);
+            transform.position = desiredPosition;
         }
     }
 
