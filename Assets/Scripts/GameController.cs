11880aa97df8bc6d0f6a146e98947d2ff581652f
AthosPap <papanikolaouathos@gmail.com>
2024-01-27 15:26:41 +0200

    added text box

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,6 +8,7 @@ namespace the_haha
         public delegate void GameOverDelegate();
         public event GameOverDelegate OnGameOver;
         private bool _isDecrementing = true;
+
         // Start is called before the first frame update
         private new void Awake()
         {
