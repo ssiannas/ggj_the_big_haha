c72a1d74a8608681ceb615a70da29d95562ea167
ssiannas <ssiannas@gmail.com>
2024-01-27 17:37:40 +0200

    Add player state and walking animations

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -8,27 +8,71 @@ namespace the_haha
     public class PlayerMovementController : MonoBehaviour
     {
         
+        enum PlayerState
+        {
+            Idle,
+            Walking,
+            Dead
+        }
+        
         public InputAction moveAction;
         [SerializeField]
         private float moveSpeed = 5.0f;
         private Rigidbody _rigidBody;
         private Vector3 _direction;
 
+        private Animator _animator;
 
+        private bool IsWalking
+        {
+            set
+            {
+                if (value)
+                {
+                    HandleWalkingState();
+                }
+                else
+                {
+                    HandleIdleState();
+                }
+            }
+        }
+        private PlayerState _playerState; 
+        
         void Start()
         {
             _rigidBody = GetComponent<Rigidbody>();
+            _animator = GetComponent<Animator>();
+            _playerState = PlayerState.Idle;
         }
 
+        private void HandleWalkingState()
+        {
+            if (_playerState == PlayerState.Walking) return;
+            _animator.SetBool("isWalking", true);
+            _playerState = PlayerState.Walking;
+        }
+        
+        private void HandleIdleState()
+        {
+            if (_playerState == PlayerState.Idle) return;
+            _animator.SetBool("isWalking", false);
+            _playerState = PlayerState.Idle;
+        }
+        
         void OnMove(InputValue pos)
         {
+           
             Vector2 inputPos = pos.Get<Vector2>();
+            
+            IsWalking = inputPos != Vector2.zero;
             //Vector3 playerPos = transform.position;
             _direction.x = inputPos.x;
             _direction.z = inputPos.y;
             _direction.Normalize();
             //_rigidBody.velocity += playerPos;
         }
+        
 
         private void FixedUpdate()
         {
