a540a01ba080f13f3186abb41b31d3d2e54f6aa3
ssiannas <ssiannas@gmail.com>
2024-01-28 15:07:42 +0200

    Move sfx

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 namespace the_haha
 {
-    public class AudioManager : MonoBehaviour
+    public class AudioManager : Singleton<AudioManager>
     {
        public List<Sound> sounds;
-       
-       
 
+       private new void Awake()
+       {
+           foreach (var s in sounds)
+           {
+              s.source = gameObject.AddComponent<AudioSource>();
+              s.source.clip = s.clip;
+              s.source.volume = s.volume;
+              s.source.pitch = s.pitch;
+              s.source.loop = s.loop;
+           }
+       }
+       
+       public void Play(string soundName)
+       {
+           var s = sounds.Find(sound => sound.soundName == soundName);
+           s.source.Play();
+       }
+       
+       public void Stop(string soundName)
+       {
+           var s = sounds.Find(sound => sound.soundName == soundName);
+           s.source.Stop();
+       }
     }
 }
